d45cf9b7c16722a6c52bb160f77f5abb7bff63e4
Ramil Badyrtenov <r.badyrtenov1@g.nsu.ru>
2024-09-30 23:32:08 +0700

    Task_1_1_3

---
+++ Task_1_1_3/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Expression e = new Add(new Number(3), new Mul(new Number(2), new Variable("x")));
+        int result = e.eval("x = 10; y = 13");
+        System.out.println(result);
+    }
+}
