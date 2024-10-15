be380fb4a8875b022a519e59163c58faea7ab5d2
ErzhanKorzhan <ErzhanKorzhan@users.noreply.github.com>
2024-10-15 08:39:09 +0000

    Deploying to gh-pages from @ ErzhanKorzhan/OOP@c098d760f958c02d90c4f46d53cf0e2823db7881 🚀

--- Task_1_1_2/type-search-index.js
+++ Task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Deck"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Player"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Deck"},{"p":"org.example","l":"Game"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Player"}]
