82eb32a016e1a43d668eaab3a6b5cf0803ab56ac
ErzhanKorzhan <ErzhanKorzhan@users.noreply.github.com>
2024-11-18 18:04:11 +0000

    Deploying to gh-pages from @ ErzhanKorzhan/OOP@c1183ac821c8b571588008306438d7b000732282 🚀

--- Task_1_2_1/type-search-index.js
+++ Task_1_2_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"org.example","l":"AdjacencyListGraph"},{"p":"org.example","l":"AdjacencyMatrixGraph"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Graph"},{"p":"org.example","l":"IncidenceMatrixGraph"},{"p":"org.example","l":"Main"}]
+typeSearchIndex = [{"p":"org.graph","l":"AdjacencyListGraph"},{"p":"org.graph","l":"AdjacencyMatrixGraph"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Graph"},{"p":"org.graph","l":"IncidenceMatrixGraph"},{"p":"org.example","l":"Main"}]
