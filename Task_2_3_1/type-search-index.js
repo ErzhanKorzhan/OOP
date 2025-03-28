2d85ba437e0fd73ca271c4d40df48b9ac77a2883
ErzhanKorzhan <ErzhanKorzhan@users.noreply.github.com>
2025-03-28 17:00:07 +0000

    Deploying to gh-pages from @ ErzhanKorzhan/OOP@ca13d058e324873d00701662631a511e79761f36 🚀

---
+++ Task_2_3_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Controller"},{"p":"org.example","l":"Direction"},{"p":"org.example","l":"GameBoard"},{"p":"org.example","l":"GameState"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Snake"}];updateSearchResults();
